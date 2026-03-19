d21e2f70d170fb7e77bbda9bb64e35aaba9b3e69
MoiFongo <moseszarza@gmail.com>
2026-03-19 13:25:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+// server.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 import express from “express”;
 import fetch from “node-fetch”;
 import crypto from “crypto”;
